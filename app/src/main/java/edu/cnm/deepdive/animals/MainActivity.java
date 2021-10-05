9c14f5dd6b9da09e25f84db5ba546ec893bf7c2b
Sergio Valdez <sergiovaldez159@yahoo.com>
2021-10-05 14:53:15 -0600

    Started custom spinner.

--- app/src/main/java/edu/cnm/deepdive/animals/MainActivity.java
+++ app/src/main/java/edu/cnm/deepdive/animals/MainActivity.java
@@ -1,6 +1,8 @@
 package edu.cnm.deepdive.animals;
 
 import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import edu.cnm.deepdive.animals.model.Animal;
@@ -11,10 +13,14 @@ import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
 
+  private Spinner animal_Selector;
+  private ArrayAdapter<Animal> adapter;
+
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+    animal_Selector = findViewById(R.id.animal_selector);
     new Retriever().start();
   }
 
@@ -29,9 +35,19 @@ public class MainActivity extends AppCompatActivity {
             .execute();
         if (response.isSuccessful()) {
           Log.d(getClass().getSimpleName(), response.body().toString());
+          List<Animal> animals = response.body();
+          runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+              adapter = new ArrayAdapter<>(MainActivity.this, android.R.layout.simple_spinner_item,
+                  animals);
+              animal_Selector.setAdapter(adapter);
+            }
+          });
         } else {
           Log.e(getClass().getName(), response.message());
         }
+
       } catch (IOException e) {
         Log.e(getClass().getName(), e.getMessage(), e);
       }
